8d86570a4cfe13e0fd61e0b26b1ef9ab42d96fec
kd1-80186 <bhiseab2016@gmail.com>
2024-02-10 02:44:55 +0530

    frontend and backend signup

---
+++ Frontend/emp-app_frontend/src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import EmployeeList from './components/EmployeesList';
+import NotFound from './components/NotFound';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddEmployee from './components/AddEmployee';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <div>
+          <Routes>
+            <Route exact path='/' element={<AddEmployee />} />
+            <Route path='/add' element={<EmployeeList />} />
+            {/* <Route path='/employees/edit/:id' element={<AddEmployee />} /> */}
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ Frontend/emp-app_frontend/src/components/config.js
@@ -0,0 +1,5 @@
+const config = {
+    serverURL: 'http://localhost:8444',
+  }
+  
+  export default config
